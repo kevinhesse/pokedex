531786d4cd2d579061d11add4e32778875aa4a4d
kevin <khesse92@gmail.com>
2018-04-02 21:58:21 -0400

    added html css and JS

--- main.js
+++ main.js
@@ -1,28 +1,39 @@
-let ul = document.querySelector("ul")
 
-class AllPokemon {
+let ul = document.querySelector("pokeOne")
+//create a class to contain selected api info
+class kevinKetchum {
   constructor () {
     this.all = []
   }
   add(info) {
     this.all.push(info)
+    //adds and pushes needed api info
   }
-}
-
-class Pokemon {
-  constructor(hp, attack, defense, abilities, name) {
-    this.hp = hp
-    this.attack = attack
-    this.defense = defense
-    this.abilities = abilities
-    this.name = name
   }
 
-}
-
+//links api to page when linked in html
 axios.get("https://pokeapi.co/api/v2/pokemon/5/")
-  .then((response) => {
-    let li = document.createElement("li")
-    let p = document.createElement("p")
-    let h1 = document.createElement("h1")
+.then((response) => {
+  let data = response.data
+
+  let name = document.createElement("li")
+  name.innerHTML = data.name
+  pokeOne.appendChild(name)
+
+  let attack = document.createElement("li")
+  attack.innerHTML = data.stats[4].base_stat
+  pokeOne.appendChild(attack)
+
+  let defense = document.createElement("li")
+  defense.innerHTML = data.stats[3].base_stat
+  pokeOne.appendChild(defense)
+
+  let abilities = document.createElement("li")
+  abilities.innerHTML = data.abilities
+  pokeOne.appendChild(abilities)
+
+  let hp = document. createElement("li")
+  hp.innerHTML = data.stats[5].base_stat
+  pokeOne.appendChild(hp)
+
   })
